ad7c75644dbbac5fc2a052fb23481c81bc62718f
hafizulf <hafizulf68@gmail.com>
2021-10-31 17:10:02 +0700

    feat(landing): create endpoint based on reference

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.use('/', (req, res) => {
+app.use(express.json())
+
+app.get('/', (req, res) => {
   res.send('Home page')
+})
+
+app.get('/dashboard', (req, res) => {
+  res.send('Dashboard Page')
+})
+
+app.get('/admin', (req, res) => {
+  res.send('Admin Page')
 })
 
 app.listen(3009)
