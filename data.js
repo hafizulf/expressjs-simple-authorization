3a65c6a8dad996aafd68cc84196ac413b2659033
hafizulf <hafizulf68@gmail.com>
2021-10-31 17:02:53 +0700

    chore: add data hardcode

---
+++ data.js
@@ -0,0 +1,18 @@
+const ROLE = {
+  ADMIN: 'admin',
+  BASIC: 'basic'
+}
+
+const users = [
+  { id: 1, name: 'skuy', role:ROLE.ADMIN },
+  { id: 2, name: 'hafizul', role:ROLE.BASIC },
+  { id: 3, name: 'furqan', role:ROLE.BASIC }
+]
+
+const projects = [
+  { id: 1, name: 'Skuy project', userId: 1 },
+  { id: 2, name: 'Hafizul project', userId: 2 },
+  { id: 3, name: 'Furqan project', userId: 3 }
+]
+
+module.exports = { ROLE, users, projects }
